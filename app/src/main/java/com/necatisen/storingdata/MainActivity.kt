d4cc529571ae40643c3f5b0528c45b56f5c27685
NECATISEN <necati.sn35@gmail.com>
2022-11-14 01:43:39 +0300

    Initial commit

---
+++ app/src/main/java/com/necatisen/storingdata/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.necatisen.storingdata
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.necatisen.storingdata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+            lateinit var sharedPreferences: SharedPreferences
+            var ageFromPreferences : Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //SharedPreferences Initialize
+        sharedPreferences = this.getSharedPreferences("com.necatisen.storingdata", Context.MODE_PRIVATE)
+
+        ageFromPreferences = sharedPreferences.getInt("age",-1)
+        if (ageFromPreferences == -1){
+            binding.textView.text = "Your Age"
+        }else{
+            binding.textView.text = "Your Age : $ageFromPreferences"
+        }
+    }
+
+    fun save(view : View) {
+
+
+
+        val myAge = binding.editTextTextPersonName.text.toString().toIntOrNull()
+        if (myAge != null){
+            binding.textView.text = "Your age:  $myAge"
+            sharedPreferences.edit().putInt("age",myAge).apply()
+        }
+
+    }
+
+    fun delete(view : View) {
+        ageFromPreferences = sharedPreferences.getInt("age",-1)
+        if(ageFromPreferences != -1){
+            sharedPreferences.edit().remove("age").apply()
+            binding.textView.text = "Your Age :"
+        }
+
+    }
+}
